6765f97f28ddbac44a9b92248bf7f3d0fd12701b
zdn-toan-tran <toan.tran@zodinet.tech>
2024-06-03 15:32:13 +0700

    fix: fix paths

--- src/location/entities/location.entity.ts
+++ src/location/entities/location.entity.ts
@@ -12,12 +12,10 @@ import {
 import { Province } from './province.entity';
 import { District } from './district.entity';
 import { Ward } from './ward.entity';
-import { SportField } from '../../../src/sport-field/entities/sport-field.entity';
-import { User } from '../../../src/user/entities/user.entity';
+import { SportField } from 'src/sport-field/entities/sport-field.entity';
+import { User } from 'src/user/entities/user.entity';
 
-@Entity(
-  {synchronize: true}
-)
+@Entity({ synchronize: true })
 export class Location {
   @PrimaryGeneratedColumn('uuid')
   id: string;

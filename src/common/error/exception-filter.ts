f7e5da6c2fd09da2bdadeb4cb087c7c6c230ed2a
zdn-quang-nguyen <quang.nguyen@zodinet.tech>
2024-06-10 10:36:56 +0700

    feat: Add exception filters for UpdateValuesMissingError and HttpException

--- src/common/error/exception-filter.ts
+++ src/common/error/exception-filter.ts
@@ -1,6 +1,7 @@
 import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { UpdateValuesMissingError } from 'typeorm';
+import { BaseResponse } from '../response/base.response';
 
 @Catch(UpdateValuesMissingError)
 export class UpdateValuesMissingErrorFilter implements ExceptionFilter {
@@ -9,12 +10,14 @@ export class UpdateValuesMissingErrorFilter implements ExceptionFilter {
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
 
-    console.log(exception);
+    const exceptionResponse = new BaseResponse(
+      null,
+      exception.message,
+      400,
+      new Date().toISOString(),
+    );
+    console.log('Exception: ', exceptionResponse, request.url);
 
-    response.status(400).json({
-      statusCode: 400,
-      timestamp: new Date().toISOString(),
-      path: request.url,
-    });
+    response.status(400).json(exceptionResponse);
   }
 }

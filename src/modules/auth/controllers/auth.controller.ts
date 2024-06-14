00f7b530511a4fc5142935a6675350721a009758
zdn-toan-tran <toan.tran@zodinet.tech>
2024-06-14 13:58:02 +0700

    refactor: remove set cookie

--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -1,11 +1,10 @@
-import { Body, Controller, HttpCode, Inject, Post, Res } from '@nestjs/common';
+import { Body, Controller, HttpCode, Inject, Post } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
-import { Response } from 'express';
 import { Public } from 'nest-keycloak-connect';
 import { API_BEARER_AUTH } from 'src/constants/constants';
+import { CreateAuthDto } from '../dto/create-auth.dto';
 import { SignInDto } from '../dto/sign-in.dto';
 import { AuthService } from '../services/auth.service';
-import { CreateAuthDto } from '../dto/create-auth.dto';
 
 @Controller({ path: '/auth', version: '1' })
 @ApiTags('auth')
@@ -16,19 +15,8 @@ export class AuthController {
   @Post('/login')
   @HttpCode(200)
   @Public()
-  async signIn(
-    @Res({ passthrough: true }) response: Response,
-    @Body() signInDto: SignInDto,
-  ) {
-    const data = await this.authService.signIn(signInDto);
-    const ENVIRONMENT = process.env.NODE_ENV || 'development';
-    response
-      .cookie('access_token', data.access_token, {
-        httpOnly: true,
-        secure: ENVIRONMENT === 'production',
-        sameSite: ENVIRONMENT === 'production' ? 'none' : 'lax',
-      })
-      .json(data);
+  async signIn(@Body() signInDto: SignInDto) {
+    return await this.authService.signIn(signInDto);
   }
 
   @Post('/sign-up')

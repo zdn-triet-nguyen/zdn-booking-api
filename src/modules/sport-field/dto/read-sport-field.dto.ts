c6efb26e30e1b168a59073b283bb31eed7af89fb
zdn-quang-nguyen <quang.nguyen@zodinet.tech>
2024-07-03 11:03:04 +0700

    feat: Add optional distance field to ReadSportFieldDto

--- src/modules/sport-field/dto/read-sport-field.dto.ts
+++ src/modules/sport-field/dto/read-sport-field.dto.ts
@@ -2,6 +2,7 @@ import { AutoMap } from '@automapper/classes';
 import { FieldEntity } from 'src/modules/field/entities/field.entity';
 import { CreateSportFieldDto } from './create-sport-field.dto';
 import { ReadSportFieldImageDto } from './sport-field-image/read-sport-field-image.dto';
+import { IsOptional } from 'class-validator';
 
 export class ReadSportFieldDto extends CreateSportFieldDto {
   @AutoMap()
@@ -15,6 +16,10 @@ export class ReadSportFieldDto extends CreateSportFieldDto {
 
   // @AutoMap()
   // location: LocationEntity;
+
+  @AutoMap()
+  @IsOptional()
+  distance?: number;
 
   @AutoMap()
   sportFieldImages: ReadSportFieldImageDto[];
